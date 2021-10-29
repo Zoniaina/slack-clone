3b9fb1e442453543f44b6ec3ac4ebf55238ee27a
zoniaina <fzoniaina@gmail.com>
2021-10-29 06:42:00 +0300

    all module update wip

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,27 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './core/users/users.module';
+import { MessagesModule } from './core/messages/messages.module';
+import { ChannelsModule } from './core/channels/channels.module';
+import { WorkspacesModule } from './core/workspaces/workspaces.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from './database/orm.config';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
-      // debug: false,
-      // playground: false,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      // sortSchema: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema/schema.gql'),
+      sortSchema: true,
+      subscriptions: {
+        'graphql-ws': true,
+      },
     }),
+    TypeOrmModule.forRoot(ormConfig()),
     UsersModule,
+    MessagesModule,
+    ChannelsModule,
+    WorkspacesModule,
   ],
 })
 export class AppModule {}
